51547cd928136ae399f3581da25283cb522ffe47
Alexander Tsarkov <pratchet1989@yandex.ru>
2023-07-20 12:01:07 +0300

    test

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require ('express');
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 app.use(express.static('./'));
 app.listen(PORT, function (){
     console.log('Example app listening on port ${PORT}!');
 });
+
+
+
+// const express = require('express');
+// const app = express();
+// // const expbs = require('MIDDLE.MESSENGER.PRAKTIKUM.YANDEX')
+
+// const PORT = 3000;
+
+// app.use(express.static('./'));
+
+// // app.engine('handlebars', expbs());
+// app.set("view engine", "handlebars");
+
+// //routing
+// // app.get('/', (req, res) => {
+// //   res.render('index');
+// // });
+
+// app.listen(PORT, function () {
+//   console.log(`Server is starting at port ${PORT}!`);
+// }); 
