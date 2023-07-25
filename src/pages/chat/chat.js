6a6158f62929408e41cf0a49596d72c711e16bfd
Alexander Tsarkov <pratchet1989@yandex.ru>
2023-07-25 14:51:22 +0300

    test

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -4,5 +4,13 @@ export const chat = ({ }) => {
   return chatTemplate({
     profile: 'Профиль',
     search: 'Поиск',
+    displayName: 'Семен Петров',
+    displayNameAlt: 'Salvatore',
+    messageDate: '10.02.2023',
+    messageTime: '10:15',
+    chatMessagePic: 'Изображение',
+    chatMessageLarge: 'реактивный объект с идентичной структурой ввода данных формы но с добавленными метаданными в каждое поле',
+    messageCounter: '10',
+
   });
 };
